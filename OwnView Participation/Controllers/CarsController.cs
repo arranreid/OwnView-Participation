5112df8479076a8fdf337e2f55b63691628c36f8
Arran Reid <arran@ou.edu>
2019-04-18 17:18:02 -0500

    View

---
+++ OwnView Participation/Controllers/CarsController.cs
@@ -0,0 +1,64 @@
+﻿using OwnView_Participation.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OwnView_Participation.Controllers
+{
+    public class CarsController : Controller
+    {
+        // GET: Cars
+        public ActionResult Index()
+        {
+            Car Isla = new Car()
+            {
+                Model = "MX-5",
+                Manufacturer = "Mazda",
+                Year = 2006,
+                Color = "Red",
+                Trim = "Sport",
+                Price = 9499.99
+            };
+
+            Car Bruce = new Car()
+            {
+                Model = "Camaro",
+                Manufacturer = "Chevrolet",
+                Year = 2017,
+                Color = "White",
+                Trim = "1LE",
+                Price = 65000
+            };
+
+            Car Scotty = new Car()
+            {
+                Model = "Ranger",
+                Manufacturer = "Ford",
+                Year = 2000,
+                Color = "Black",
+                Trim = "XLT",
+                Price = 2699.87
+            };
+
+            Car Flavia = new Car()
+            {
+                Model = "F-Type",
+                Manufacturer = "Jaguar",
+                Year = 2018,
+                Color = "Orange",
+                Trim = "R-Sport",
+                Price = 94000
+            };
+
+            List<Car> cr = new List<Car>();
+            cr.Add(Isla);
+            cr.Add(Bruce);
+            cr.Add(Scotty);
+            cr.Add(Flavia);
+
+            return View();
+        }
+    }
+}
---
+++ OwnView Participation/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OwnView_Participation.Models
+{
+    public class Car
+    {
+        public string Model;
+        public string Manufacturer;
+        public int Year;
+        public string Color;
+        public string Trim;
+        public double Price;
+    }
+}
