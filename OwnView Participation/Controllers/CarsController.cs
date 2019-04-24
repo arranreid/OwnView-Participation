833d78546d7b0f7cd81914608dd10e6f76dd69ee
Arran Reid <arran@ou.edu>
2019-04-23 19:08:47 -0500

    Fin

--- OwnView Participation/Controllers/CarsController.cs
+++ OwnView Participation/Controllers/CarsController.cs
@@ -15,7 +15,7 @@ namespace OwnView_Participation.Controllers
             Car Isla = new Car()
             {
                 Model = "MX-5",
-                Image = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwj6javo29rhAhXpsVQKHVZ7DYMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.drive.co.uk%2Fupdated-2019-mazda-mx-5-no-excuse-needed%2F&psig=AOvVaw3JGy3YUNSnGpQALWbfji7C&ust=1555713839029479",
+                Image = "http://www.blogcdn.com/www.autoblog.com/media/2008/06/03_abgarage_08_mx5_450op.jpg",
                 Manufacturer = "Mazda",
                 Year = 2006,
                 Color = "Red",
@@ -26,7 +26,7 @@ namespace OwnView_Participation.Controllers
             Car Bruce = new Car()
             {
                 Model = "Camaro",
-                Image = "https://www.google.com/url?sa=i&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjW8pL-29rhAhXP0J8KHSj3CUMQjRx6BAgBEAU&url=https%3A%2F%2Fwww.caranddriver.com%2Ffeatures%2Fa15101319%2Fchevrolet-camaro-ss-1le-at-lightning-lap-2016-feature%2F&psig=AOvVaw0d3fS0y1k11O6hu0Nn_oSf&ust=1555713877762579",
+                Image = "https://cdn.motor1.com/images/mgl/jAjO7/s1/2017-chevrolet-camaro-1le-first-drive.jpg",
                 Manufacturer = "Chevrolet",
                 Year = 2017,
                 Color = "White",
